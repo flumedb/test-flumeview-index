13bc27a2bdc14c435c0f0b9f9fac31f0c57ea831
Dominic Tarr <dominic.tarr@gmail.com>
2017-08-12 16:44:19 +1200

    test reloading the view

--- index.js
+++ index.js
@@ -7,28 +7,48 @@ var data = [
   {key: Math.random(), value: {foo: true, bar: Date.now()}}
 ]
 
-module.exports = function (db) {
+module.exports = function (create) {
+
+  var seed = Date.now()
+  var db = create(seed)
 
   tape('simple', function (t) {
     db.append(data, function (err, m) {
       if(err) throw err
-      db.index.get(data[0].key, function (err, value) {
-        if(err) throw err
-        t.deepEqual(value, data[0])
-        db.index.get(data[1].key, function (err, value) {
-          if(err) throw err
-          t.deepEqual(value, data[1])
-          db.index.get(data[2].key, function (err, value) {
-            if(err) throw err
-            t.deepEqual(value, data[2])
-            t.end()
-          })
-        })
-      })
+      t.end()
     })
   })
 
+  function test (t) {
+    db.index.get(data[0].key, function (err, value) {
+      if(err) throw err
+      t.deepEqual(value, data[0])
+      db.index.get(data[1].key, function (err, value) {
+        if(err) throw err
+        t.deepEqual(value, data[1])
+        db.index.get(data[2].key, function (err, value) {
+          if(err) throw err
+          t.deepEqual(value, data[2])
+          t.end()
+        })
+      })
+    })
+  }
+
+  tape('test', test)
+
+  tape('reload', function (t) {
+    var db = create(seed)
+    t.end()
+  })
+
+  tape('retest', test)
+
 }
+
+
+
+
 
 
 
